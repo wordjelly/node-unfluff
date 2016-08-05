1583fe08d215499d6c08a67594bca7e8bbd09503
bhargav <bhargav.r.raut@gmail.com>
2016-08-05 10:57:23 +0530

    mod cleanParagraphText to return rawInput, modified convertToText function to skip adding spaces between sentences if they dont exist

--- test/mod.js
+++ test/mod.js
@@ -1,3 +1,9 @@
 extractor = require('../lib/unfluff.js');
-//data = extractor(my_html_data);
-//console.log(data.text);
+var request = require("request");
+request("http://www.firstpost.com/politics/statehood-for-delhi-or-not-arvind-kejriwal-has-been-relegated-to-the-role-of-mayor-2936448.html", function(error, response, body) {
+  d = extractor(body);
+  console.log(d.text);
+  console.log("last text is:" + d.text[d.text.length - 1]);
+  //use following jquery function to get the node on the page
+  //$(":contains(d.text[d.text.length - 1])"), and the last node is the deepest or childest node to have the desired text.
+});
